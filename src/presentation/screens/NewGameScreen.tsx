0ac160726794d10feda246b44ec9f2b94fedf1b4
Joel Toribio <joeltoribiopolanco@gmail.com>
2025-12-17 23:41:39 -0400

    feat: cambiando modo local a ia

--- src/presentation/screens/NewGameScreen.tsx
+++ src/presentation/screens/NewGameScreen.tsx
@@ -56,7 +56,7 @@ export default function NewGameScreen() {
             return;
         }
 
-        navigation.navigate('Loading', { mode: 'local' })
+        navigation.navigate('Loading', { mode: 'ia' })
     }
 
     return (
